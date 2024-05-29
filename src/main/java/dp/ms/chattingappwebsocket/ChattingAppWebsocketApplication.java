7ceaa797c3f512dbbfb9fd057e4cdd18aafe0fab
Murad Bashirli <mbashirlee@gmail.com>
2024-05-29 16:32:49 -0400

    Initial commit

---
+++ src/main/java/dp/ms/chattingappwebsocket/ChattingAppWebsocketApplication.java
@@ -0,0 +1,13 @@
+package dp.ms.chattingappwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingAppWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingAppWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dp/ms/chattingappwebsocket/ChattingAppWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dp.ms.chattingappwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingAppWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
